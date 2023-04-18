f54b7aa56a205d974d6d525911c23cd7edf16ced
SyrGitHub <129231566+SyriusGit@users.noreply.github.com>
2023-04-18 20:54:04 -0300

    Exercicios array

---
+++ Exercicios_Arrays/script.js
@@ -0,0 +1,17 @@
+let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3,4,5,6,7,8,9,10,11,12,13]
+console.log('c. ', array.length) //11
+
+let i = 0 //Índice
+console.log('d. ', array[i]) //3
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
